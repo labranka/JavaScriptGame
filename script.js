6ea0c373061ff795bb0db75d562d4192ee5d3d32
Branka <brankarbc96@gmail.com>
2022-02-09 01:26:26 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Selektujem skorove i postavljam skorove na 0
+const score0El = document.querySelector('#score--0');
+//drugi nacin selektovanja
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
